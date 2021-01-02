98f35c4b601e556cc99c914b62ced707a7a5559a
polo123456789 <pabsan02@gmail.com>
2021-01-01 19:49:57 -0600

    (*)<All> Comit inicial

---
+++ src/cmm.hpp
@@ -0,0 +1,12 @@
+#ifndef CMM_MAIN_INCLUDE_FILE_HPP
+#define CMM_MAIN_INCLUDE_FILE_HPP
+
+#include <string>
+
+namespace cmm {
+
+std::string parce(const std::string &source);
+
+}
+
+#endif
---
+++ src/cmm/cmm.cpp
@@ -0,0 +1,7 @@
+#include <cmm.hpp>
+
+namespace cmm {
+    std::string parce(const std::string& source) {
+        return "";
+    }
+}
