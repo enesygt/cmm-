0d7562e3fa61fc0bdfba46605bdce73fba7cec36
polo123456789 <pabsan02@gmail.com>
2021-01-03 17:06:14 -0600

    (+)<inlines> Prototypes

---
+++ src/cmm/inlines.cpp
@@ -0,0 +1,28 @@
+#include <cmm/inlines.hpp>
+#include <regex>
+
+namespace cmm {
+
+void process_inlines(std::string &source) {}
+
+void process_code_spans(std::string &source) {
+
+}
+
+void process_emphasis(std::string &source) {
+
+}
+
+void process_strong_emphasis(std::string &souce) {
+
+}
+
+void process_links(std::string &souce) {
+
+}
+
+void process_images(std::string &souce) {
+
+}
+
+} // namespace cmm
--- src/cmm/inlines.hpp
+++ src/cmm/inlines.hpp
@@ -1,8 +1,17 @@
 #ifndef CMM_INLINES_HPP
 #define CMM_INLINES_HPP
 
+#include <string>
+
 namespace cmm {
 
-}
+void process_inlines(std::string &source);
+void process_code_spans(std::string &source);
+void process_emphasis(std::string &source);
+void process_strong_emphasis(std::string &souce);
+void process_links(std::string &souce);
+void process_images(std::string &souce);
+
+} // namespace cmm
 
 #endif
