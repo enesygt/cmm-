a303288edf27940fe00e95ce5a997c97b32dc35d
polo123456789 <pabsan02@gmail.com>
2021-01-03 11:57:03 -0600

    (+)<main> Prototipo de el programa

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <cmm/cmm.hpp>
+
+int main(int argc, char *argv[]) {
+    //cmm::config::parce_cmd_args(argc, argv);
+    //auto& out = cmm::config::out_stream;
+    //auto& in  = cmm::config::in_stream;
+    //out << cmm::parce(in);
+    //return 0;
+}
