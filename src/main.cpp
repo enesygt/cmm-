edb3670da25104fc18e21f0485f952b18906dc2a
polo123456789 <pabsan02@gmail.com>
2021-01-08 10:30:40 -0600

    (*)<main> Por lineas para el debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
 #include <cmm/cmm.hpp>
 
+constexpr bool testing_inlines = true;
+
 int main(int argc, char *argv[]) {
     try {
-        try {
-            cmm::config::parce_cmd_args(argc, argv);
-        } catch (const cmm::config::missing_cmdline_args &e) {
-            std::cerr << e.what() << '\n';
-            return 1;
-        }
+        if constexpr (testing_inlines) {
 
-        std::ostream &out = cmm::config::get_out_stream();
-        std::istream &in = cmm::config::get_input_stream();
+            std::string s;
+            while (std::getline(std::cin, s)) {
+                std::cout << cmm::parce(s) << '\n';
+            }
 
-        cmm::log << "---- Debug messages enabled ----\n";
+        } else {
 
-        if (cmm::config::just_help_message) {
-            out << cmm::config::help_message << '\n';
+            try {
+                cmm::config::parce_cmd_args(argc, argv);
+            } catch (const cmm::config::missing_cmdline_args &e) {
+                std::cerr << e.what() << '\n';
+                return 1;
+            }
+
+            std::ostream &out = cmm::config::get_out_stream();
+            std::istream &in = cmm::config::get_input_stream();
+
+            cmm::log << "---- Debug messages enabled ----\n";
+
+            if (cmm::config::just_help_message) {
+                out << cmm::config::help_message << '\n';
+                return 0;
+            }
+
+            std::string source = cmm::full_stream_read(in);
+            cmm::log << "Read from stream:\n";
+            cmm::log << source << '\n';
+
+            out << cmm::parce(source);
             return 0;
         }
-
-        std::string source = cmm::full_stream_read(in);
-        cmm::log << "Read from stream:\n";
-        cmm::log << source << '\n';
-
-        out << cmm::parce(source);
-        return 0;
     } catch (const std::exception &e) {
         std::cerr << e.what() << '\n';
         return 1;
