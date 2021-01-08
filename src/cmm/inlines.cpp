f140041955f339f6ccc0aff5eb2b5539d35a2aec
polo123456789 <pabsan02@gmail.com>
2021-01-08 10:31:30 -0600

    (*)<inlines> Vamos a probar hacerlo sin regex

--- src/cmm/inlines.cpp
+++ src/cmm/inlines.cpp
@@ -3,30 +3,26 @@
 
 namespace cmm {
 
-void process_inlines(std::string &source) {
+void process_strong_emphasis(std::string &source) {
+    std::regex emphasis(R"(([^\\_*]*)[_*]{2}([^_*]*[^_*\\])[_*]{2})");
+    std::string replacement("$1<strong>$2</strong>$3");
 
+    std::string result = std::regex_replace(source, emphasis, replacement);
+
+    source = std::move(result);
+}
+
+void process_emphasis(std::string &source) {
+    std::regex emphasis(R"(([^\\_*]*)[_*]([^_*]*[^_*\\])[_*])");
+    std::string replacement("$1<em>$2</em>$3");
+
+    std::string result = std::regex_replace(source, emphasis, replacement);
+
+    source = std::move(result);
 }
 
 void process_code_spans(std::string &source) {
 
-}
-
-void process_emphasis(std::string &source) {
-    std::regex emphasis(R"([_*]([^_*]+)[_*])");
-    std::string replacement("<em>$1</em>");
-
-    std::string result = std::regex_replace(source, emphasis, replacement);
-
-    source = std::move(result);
-}
-
-void process_strong_emphasis(std::string &source) {
-    std::regex emphasis(R"([_*]{2}([^_*]+)[_*]{2})");
-    std::string replacement("<strong>$1</strong>");
-
-    std::string result = std::regex_replace(source, emphasis, replacement);
-
-    source = std::move(result);
 }
 
 void process_links(std::string &source) {
--- src/cmm/inlines.hpp
+++ src/cmm/inlines.hpp
@@ -1,16 +1,22 @@
 #ifndef CMM_INLINES_HPP
 #define CMM_INLINES_HPP
 
+#include <cmm/config.hpp>
 #include <string>
 
 namespace cmm {
 
-void process_inlines(std::string &source);
-void process_code_spans(std::string &source);
-void process_emphasis(std::string &source);
 void process_strong_emphasis(std::string &source);
+void process_emphasis(std::string &source);
+void process_code_spans(std::string &source);
 void process_links(std::string &source);
 void process_images(std::string &source);
+
+void inline process_inlines(std::string &source) {
+    process_strong_emphasis(source);
+    process_emphasis(source);
+}
+
 
 } // namespace cmm
 
