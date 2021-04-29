9eb3d6aa7e876067e27a1cdb07364a97f4bbe8b7
polo123456789 <pabsan02@gmail.com>
2021-04-29 09:10:22 -0600

    (*)<main> A la normalidad

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <cmm/cmm.hpp>
 
-constexpr bool testing_inlines = true;
-
 int main(int argc, char *argv[]) {
     try {
         cmm::config::parce_cmd_args(argc, argv);
@@ -14,30 +12,20 @@ int main(int argc, char *argv[]) {
     cmm::log << "---- Debug messages enabled ----\n";
 
     try {
-        if constexpr (testing_inlines) {
 
-            std::string s;
-            while (std::getline(std::cin, s)) {
-                std::cout << cmm::parce(s) << '\n';
-            }
+        std::ostream &out = cmm::config::get_out_stream();
+        std::istream &in = cmm::config::get_input_stream();
 
-        } else {
-
-            std::ostream &out = cmm::config::get_out_stream();
-            std::istream &in = cmm::config::get_input_stream();
-
-            if (cmm::config::just_help_message) {
-                out << cmm::config::help_message << '\n';
-                return 0;
-            }
-
-            std::string source = cmm::full_stream_read(in);
-            cmm::log << "Read from stream:\n";
-            cmm::log << source << '\n';
-
-            out << cmm::parce(source);
+        if (cmm::config::just_help_message) {
+            out << cmm::config::help_message << '\n';
             return 0;
         }
+
+        std::string source = cmm::full_stream_read(in);
+
+        out << cmm::parce(source);
+        return 0;
+
     } catch (const std::exception &e) {
         std::cerr << e.what() << '\n';
         return 1;
