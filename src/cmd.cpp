64e9da471306eabc327233a065a9c83efcd70fa4
polo123456789 <pabsan02@gmail.com>
2021-05-10 16:11:06 -0600

    (*)<parser/cmd> Missing noexcept in .cpp

--- src/cmd.cpp
+++ src/cmd.cpp
@@ -25,39 +25,39 @@ void parce_cmd_args(int argc, char *argv[]) {
 
     for (int i = 0; i < argc; i++) {
         // Debug messages
-        if (std::strcmp("-d", argv_at(i)) == 0
-            || std::strcmp("--debug-messages", argv_at(i)) == 0) {
+        if (strcmp("-d", argv_at(i)) == 0
+            || strcmp("--debug-messages", argv_at(i)) == 0) {
 
             cmm::config::conf.debug(true);
         }
 
         // Input file
-        else if (std::strcmp("-f", argv_at(i)) == 0
-                 || std::strcmp("--input-file", argv_at(i)) == 0) {
+        else if (strcmp("-f", argv_at(i)) == 0
+                 || strcmp("--input-file", argv_at(i)) == 0) {
 
             const char *in = argv_at(i + 1);
             // Do nothing if stream is stdin
-            if (std::strcmp("-", in) != 0) {
+            if (strcmp("-", in) != 0) {
                 imp::possible_input_stream.open(argv_at(i + 1));
                 imp::in_stream = &imp::possible_input_stream;
             }
         }
 
         // Output file
-        else if (std::strcmp("-o", argv_at(i)) == 0
-                 || std::strcmp("--output-file", argv_at(i)) == 0) {
+        else if (strcmp("-o", argv_at(i)) == 0
+                 || strcmp("--output-file", argv_at(i)) == 0) {
 
             const char *out = argv_at(i + 1);
             // Do nothing if stream is stdout
-            if (std::strcmp("-", out) != 0) {
+            if (strcmp("-", out) != 0) {
                 imp::possible_output_stream.open(argv_at(i + 1));
                 imp::out_stream = &imp::possible_output_stream;
             }
         }
 
         // Help message
-        else if (std::strcmp("-h", argv_at(i)) == 0
-                 || std::strcmp("--help", argv_at(i)) == 0) {
+        else if (strcmp("-h", argv_at(i)) == 0
+                 || strcmp("--help", argv_at(i)) == 0) {
             just_help_message = true;
             return;
         }
@@ -72,6 +72,6 @@ std::ostream &get_out_stream(void) {
     return *imp::out_stream;
 }
 
-const char *missing_cmdline_args::what() const {
+const char *missing_cmdline_args::what() const noexcept {
     return "Missing comand line arguments";
 }
