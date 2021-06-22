a412cf20cd358d49cbd6db5191cbe46a4c5f96a8
polo123456789 <pabsan02@gmail.com>
2021-06-22 17:30:20 -0600

    (+)<lib> Defined text blocks

---
+++ src/cmm-lib/cmm-lib/text_block.cpp
@@ -0,0 +1,7 @@
+#include <cmm/text_block.hpp>
+
+using cmm::text_block;
+
+std::vector<text_block> cmm::separate_blocks(const std::string& source) {
+
+}
--- src/cmm-lib/include/cmm/text_block.hpp
+++ src/cmm-lib/include/cmm/text_block.hpp
@@ -7,10 +7,33 @@
 namespace cmm {
 
 /**
- * 
+ * A series of lines that end in two '\n'.
+ *
+ * Basically a paragraph.
+ *
+ * ~~~
+ * Lorem ipsum dolor sit amet consectetur adipisicing elit. |
+ * Facere provident velit, quibusdam, aliquam earum         |
+ * corporis totam mollitia fugiat in vitae, placeat         |<- This is block 1
+ * quaerat! Consequuntur eveniet quos a aliquid perferendis |
+ * illo!  Earum!                                            |
+ *
+ * Lorem ipsum dolor sit amet consectetur adipisicing elit. |
+ * Facere provident velit, quibusdam, aliquam earum         |
+ * corporis totam mollitia fugiat in vitae, placeat         |<- This is block 2
+ * quaerat! Consequuntur eveniet quos a aliquid perferendis |
+ * illo!  Earum!                                            |
+ * ~~~
  */
-using text_block = std::vector<std::string>;
+using text_block = std::string;
 
-}
+/**
+ * Separates the source in text blocks.
+ *
+ * @see cmm::text_block
+ */
+std::vector<text_block> separate_blocks(const std::string& source);
+
+} // namespace cmm
 
 #endif
