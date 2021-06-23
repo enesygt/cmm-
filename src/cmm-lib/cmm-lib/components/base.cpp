31951ee3afa5b9ef5b7fe818e69b53425735144c
polo123456789 <pabsan02@gmail.com>
2021-06-22 19:29:21 -0600

    (~)<lib/components/base> Finished refactoring
    See 09bb5c8 for details

--- src/cmm-lib/cmm-lib/components/base.cpp
+++ src/cmm-lib/cmm-lib/components/base.cpp
@@ -1,6 +1,7 @@
 #include <cmm/components/base.hpp>
 
-namespace cmm {
+using cmm::markdown_component;
+using cmm::markdown_component_type;
 
 [[nodiscard]] markdown_component_type markdown_component::get_type() {
     return this->type;
@@ -9,5 +10,3 @@ namespace cmm {
 void markdown_component::set_type(markdown_component_type c) {
     this->type = c;
 }
-
-} // namespace cmm
