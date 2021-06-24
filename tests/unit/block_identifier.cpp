239ea6dcb2a74ea65e427db02bd27d2e82cb8ed4
polo123456789 <pabsan02@gmail.com>
2021-06-24 12:04:54 -0600

    (+)<components> Test for filtering ready

--- tests/unit/block_identifier.cpp
+++ tests/unit/block_identifier.cpp
@@ -21,7 +21,6 @@ TEST_CASE("cmm::identify_block_type", "[text_block]") {
      */
 
     SECTION("Atx Headings") {
-
         const std::string                  source(raw_source_atx_headings);
         const std::vector<cmm::text_block> blocks =
             cmm::separate_blocks(source);
@@ -33,7 +32,6 @@ TEST_CASE("cmm::identify_block_type", "[text_block]") {
     }
 
     SECTION("Setext Headins") {
-
         const std::string                  source(raw_source_setext_heading);
         const std::vector<cmm::text_block> blocks =
             cmm::separate_blocks(source);
@@ -44,11 +42,110 @@ TEST_CASE("cmm::identify_block_type", "[text_block]") {
         }
     }
 
-    SECTION("Indented code blocks") {}
-    SECTION("Fenced code block") {}
-    SECTION("Html Block") {}
-    SECTION("Block quote") {}
-    SECTION("List") {}
-    SECTION("Thematic Break") {}
-    SECTION("Paragraph") {}
+    SECTION("Indented code blocks") {
+        const std::string                  source(raw_source_indented_codeblock);
+        const std::vector<cmm::text_block> blocks =
+            cmm::separate_blocks(source);
+
+        for (const auto &b : blocks) {
+            const auto type = cmm::identify_block_type(b);
+            CHECK(type == cmm::markdown_component_type::indented_code_block);
+        }
+    }
+
+    SECTION("Fenced code block openings") {
+        const std::string                  source(raw_source_fenced_code_block_opening);
+        const std::vector<cmm::text_block> blocks =
+            cmm::separate_blocks(source);
+
+        for (const auto &b : blocks) {
+            const auto type = cmm::identify_block_type(b);
+            CHECK(type == cmm::markdown_component_type::fenced_code_block_opening);
+        }
+    }
+
+    SECTION("Fenced code block closings") {
+        const std::string                  source(raw_source_fenced_code_block_closing);
+        const std::vector<cmm::text_block> blocks =
+            cmm::separate_blocks(source);
+
+        for (const auto &b : blocks) {
+            const auto type = cmm::identify_block_type(b);
+            CHECK(type == cmm::markdown_component_type::fenced_code_block_closing);
+        }
+
+    }
+
+    // Pending for future versions
+    // SECTION("Html Block") {
+    //     const std::string                  source();
+    //     const std::vector<cmm::text_block> blocks =
+    //         cmm::separate_blocks(source);
+
+    //     for (const auto &b : blocks) {
+    //         const auto type = cmm::identify_block_type(b);
+    //         CHECK(type == cmm::markdown_component_type::undefined);
+    //     }
+
+    // }
+
+    SECTION("Block quote") {
+        const std::string                  source(raw_source_block_quote);
+        const std::vector<cmm::text_block> blocks =
+            cmm::separate_blocks(source);
+
+        for (const auto &b : blocks) {
+            const auto type = cmm::identify_block_type(b);
+            CHECK(type == cmm::markdown_component_type::block_quote);
+        }
+
+    }
+
+    SECTION("Ordered List") {
+        const std::string                  source(raw_source_ordered_list);
+        const std::vector<cmm::text_block> blocks =
+            cmm::separate_blocks(source);
+
+        for (const auto &b : blocks) {
+            const auto type = cmm::identify_block_type(b);
+            CHECK(type == cmm::markdown_component_type::ordered_list);
+        }
+
+    }
+
+    SECTION("Unordered List") {
+        const std::string                  source(raw_source_unordered_list);
+        const std::vector<cmm::text_block> blocks =
+            cmm::separate_blocks(source);
+
+        for (const auto &b : blocks) {
+            const auto type = cmm::identify_block_type(b);
+            CHECK(type == cmm::markdown_component_type::unordered_list);
+        }
+
+    }
+
+    SECTION("Thematic Break") {
+        const std::string                  source(raw_source_thematic_break);
+        const std::vector<cmm::text_block> blocks =
+            cmm::separate_blocks(source);
+
+        for (const auto &b : blocks) {
+            const auto type = cmm::identify_block_type(b);
+            CHECK(type == cmm::markdown_component_type::thematic_break);
+        }
+
+    }
+
+    SECTION("Paragraph") {
+        const std::string                  source(raw_source_paragraph);
+        const std::vector<cmm::text_block> blocks =
+            cmm::separate_blocks(source);
+
+        for (const auto &b : blocks) {
+            const auto type = cmm::identify_block_type(b);
+            CHECK(type == cmm::markdown_component_type::paragraph);
+        }
+
+    }
 }
