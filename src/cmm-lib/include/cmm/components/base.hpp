6fab99b4e6bc0fb67480937afe034710fce154c8
polo123456789 <pabsan02@gmail.com>
2021-06-24 11:57:51 -0600

    (-)<lib/components/base> Added missing virtual dctor.

--- src/cmm-lib/include/cmm/components/base.hpp
+++ src/cmm-lib/include/cmm/components/base.hpp
@@ -7,11 +7,14 @@
 
 namespace cmm {
 
-class markdown_component {
+class markdown_component { // NOLINT It has a virtual destructor in case one of
+                           // the classes that inherits it needs a destructor
+                           // but there is no need for copy, and move.
    public:
     virtual void parce_block(const std::string &source) = 0;
     [[nodiscard]] virtual std::string to_html() const = 0;
     [[nodiscard]] markdown_component_type      get_type();
+    virtual ~markdown_component() = default;
 
    protected:
     void set_type(markdown_component_type c);
