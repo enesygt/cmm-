94bc7300efb16dc372a29acd867d89e78b06b673
polo123456789 <pabsan02@gmail.com>
2021-06-26 11:34:43 -0600

    (+~)<tests/block_identifier> DRY, and syntax error for atx heading

--- tests/unit/block_identifier.cpp
+++ tests/unit/block_identifier.cpp
@@ -1,3 +1,4 @@
+#include<cmm/syntax_error.hpp>
 #include <cmm/text_block.hpp>
 
 #include "catch2/catch.hpp"
@@ -7,78 +8,78 @@
 // NOLINTNEXTLINE The complexity is due to the catch macros
 TEST_CASE("cmm::identify_block_type", "[text_block]") {
 
-    /*     SECTION ("Template") {
-     *         const std::string source(raw_source_type);
-     *         const std::vector<cmm::text_block> blocks =
-     *             cmm::separate_blocks(source);
-     *
-     *         for (const auto& b : blocks) {
-     *             const auto type = cmm::identify_block_type(b);
-     *             CHECK(type == cmm::markdown_component_type::undefined);
-     *         }
-     *      }
-     *
-     */
+    auto check_that_correctly_identifies =
+        [](const char *raw_source, cmm::markdown_component_type expected) {
+            const std::string                  source(raw_source);
+            const std::vector<cmm::text_block> blocks =
+                cmm::separate_blocks(source);
+
+            for (const auto &b : blocks) {
+                const auto type = cmm::identify_block_type(b);
+                INFO(b) // No ;, seems like catch takes care of that
+                CHECK(type == expected);
+            }
+        };
+
+    auto check_syntax_errors =
+        [](const char* raw_source) {
+            const std::string                  source(raw_source);
+            const std::vector<cmm::text_block> blocks =
+                cmm::separate_blocks(source);
+
+            for (const auto &b : blocks) {
+                INFO(b) // No ;, seems like catch takes care of that
+                CHECK_THROWS_AS(cmm::identify_block_type(b), cmm::syntax_error);
+            }
+        };
 
     SECTION("Atx Headings") {
-        const std::string                  source(raw_source_atx_headings);
-        const std::vector<cmm::text_block> blocks =
-            cmm::separate_blocks(source);
-
-        for (const auto &b : blocks) {
-            const auto type = cmm::identify_block_type(b);
-            INFO(b) // No ;, seems like catch takes care of that
-            CHECK(type == cmm::markdown_component_type::atx_headings);
+        SECTION("Optimal cases") {
+            check_that_correctly_identifies(
+                raw_source_atx_headings,
+                cmm::markdown_component_type::atx_headings);
+        }
+        SECTION("Syntax Errors") {
+            const char *src = R"(
+# The next line is just formed by spaces
+                                        
+# That should throw, because that
+# might not be the intent of the user.
+            )";
+            check_syntax_errors(src);
         }
     }
 
     SECTION("Setext Headins") {
-        const std::string                  source(raw_source_setext_heading);
-        const std::vector<cmm::text_block> blocks =
-            cmm::separate_blocks(source);
-
-        for (const auto &b : blocks) {
-            const auto type = cmm::identify_block_type(b);
-            INFO(b) // No ;, seems like catch takes care of that
-            CHECK(type == cmm::markdown_component_type::setext_heading);
+        SECTION("Optimal cases") {
+            check_that_correctly_identifies(
+                raw_source_setext_heading,
+                cmm::markdown_component_type::setext_heading);
         }
     }
 
     SECTION("Indented code blocks") {
-        const std::string                  source(raw_source_indented_codeblock);
-        const std::vector<cmm::text_block> blocks =
-            cmm::separate_blocks(source);
-
-        for (const auto &b : blocks) {
-            const auto type = cmm::identify_block_type(b);
-            INFO(b) // No ;, seems like catch takes care of that
-            CHECK(type == cmm::markdown_component_type::indented_code_block);
+        SECTION("Optimal cases") {
+            check_that_correctly_identifies(
+                raw_source_indented_codeblock,
+                cmm::markdown_component_type::indented_code_block);
         }
     }
 
     SECTION("Fenced code block openings") {
-        const std::string                  source(raw_source_fenced_code_block_opening);
-        const std::vector<cmm::text_block> blocks =
-            cmm::separate_blocks(source);
-
-        for (const auto &b : blocks) {
-            const auto type = cmm::identify_block_type(b);
-            INFO(b) // No ;, seems like catch takes care of that
-            CHECK(type == cmm::markdown_component_type::fenced_code_block_opening);
+        SECTION("Optimal cases") {
+            check_that_correctly_identifies(
+                raw_source_fenced_code_block_opening,
+                cmm::markdown_component_type::fenced_code_block_opening);
         }
     }
 
     SECTION("Fenced code block closings") {
-        const std::string                  source(raw_source_fenced_code_block_closing);
-        const std::vector<cmm::text_block> blocks =
-            cmm::separate_blocks(source);
-
-        for (const auto &b : blocks) {
-            const auto type = cmm::identify_block_type(b);
-            INFO(b) // No ;, seems like catch takes care of that
-            CHECK(type == cmm::markdown_component_type::fenced_code_block_closing);
+        SECTION("Optimal cases") {
+            check_that_correctly_identifies(
+                raw_source_fenced_code_block_closing,
+                cmm::markdown_component_type::fenced_code_block_closing);
         }
-
     }
 
     // Pending for future versions
@@ -95,67 +96,42 @@ TEST_CASE("cmm::identify_block_type", "[text_block]") {
     // }
 
     SECTION("Block quote") {
-        const std::string                  source(raw_source_block_quote);
-        const std::vector<cmm::text_block> blocks =
-            cmm::separate_blocks(source);
-
-        for (const auto &b : blocks) {
-            const auto type = cmm::identify_block_type(b);
-            INFO(b) // No ;, seems like catch takes care of that
-            CHECK(type == cmm::markdown_component_type::block_quote);
+        SECTION("Optimal cases") {
+            check_that_correctly_identifies(
+                raw_source_block_quote,
+                cmm::markdown_component_type::block_quote);
         }
-
     }
 
     SECTION("Ordered List") {
-        const std::string                  source(raw_source_ordered_list);
-        const std::vector<cmm::text_block> blocks =
-            cmm::separate_blocks(source);
-
-        for (const auto &b : blocks) {
-            const auto type = cmm::identify_block_type(b);
-            INFO(b) // No ;, seems like catch takes care of that
-            CHECK(type == cmm::markdown_component_type::ordered_list);
+        SECTION("Optimal cases") {
+            check_that_correctly_identifies(
+                raw_source_ordered_list,
+                cmm::markdown_component_type::ordered_list);
         }
-
     }
 
     SECTION("Unordered List") {
-        const std::string                  source(raw_source_unordered_list);
-        const std::vector<cmm::text_block> blocks =
-            cmm::separate_blocks(source);
-
-        for (const auto &b : blocks) {
-            const auto type = cmm::identify_block_type(b);
-            INFO(b) // No ;, seems like catch takes care of that
-            CHECK(type == cmm::markdown_component_type::unordered_list);
+        SECTION("Optimal cases") {
+            check_that_correctly_identifies(
+                raw_source_unordered_list,
+                cmm::markdown_component_type::unordered_list);
         }
-
     }
 
     SECTION("Thematic Break") {
-        const std::string                  source(raw_source_thematic_break);
-        const std::vector<cmm::text_block> blocks =
-            cmm::separate_blocks(source);
-
-        for (const auto &b : blocks) {
-            const auto type = cmm::identify_block_type(b);
-            INFO(b) // No ;, seems like catch takes care of that
-            CHECK(type == cmm::markdown_component_type::thematic_break);
+        SECTION("Optimal cases") {
+            check_that_correctly_identifies(
+                raw_source_thematic_break,
+                cmm::markdown_component_type::thematic_break);
         }
-
     }
 
     SECTION("Paragraph") {
-        const std::string                  source(raw_source_paragraph);
-        const std::vector<cmm::text_block> blocks =
-            cmm::separate_blocks(source);
-
-        for (const auto &b : blocks) {
-            const auto type = cmm::identify_block_type(b);
-            INFO(b) // No ;, seems like catch takes care of that
-            CHECK(type == cmm::markdown_component_type::paragraph);
+        SECTION("Optimal cases") {
+            check_that_correctly_identifies(
+                raw_source_paragraph,
+                cmm::markdown_component_type::paragraph);
         }
-
     }
 }
